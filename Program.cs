0ed78fc27f61e1ca88a78101bf6fb716ce149812
Joshua <moyosoreolabisi07@gmail.com>
2022-04-07 00:36:53 +0100

    Lab 2

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace week1b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare 10 variables with their different data type
+            //1. string variable
+            string name = "Joshua Akinola";
+            Console.WriteLine("myname");
+
+            //2. integar variable
+            int wholenumber = 15000;
+            Console.WriteLine("wholenumber");
+
+            //3. Long
+            long Wholenumber = 1567890L;
+            Console.WriteLine("Wholenumber");
+
+            //4. Float
+            float serial = 2345636F;
+            Console.WriteLine("serial");
+
+            //5. Boolean
+            bool isthenealgood = true;
+            bool isthemeatsoft = false;
+            Console.WriteLine("isthenealgood");
+            Console.WriteLine("isthemeatsoft");
+
+            //6. character
+            char grade = 'B';
+            Console.WriteLine("grade");
+
+            //6. Object
+            object item = 100;
+            Console.WriteLine("item");
+
+            //7. variable
+            var goods = "desktop";
+            Console.WriteLine("goods");
+
+            //8. Dynamic
+            dyn fish + =1000;
+            Console.WriteLine("fish");
+
+
+
+        }
+    }
+}
